69bc37f84ef9e0443efb9f676a501306e91be4d7
heguzzq <825449399@qq.com>
2024-01-05 10:54:19 +0800

    提交新文件

--- 抽数/7001青岛积分清零.sql
+++ 抽数/7001青岛积分清零.sql
@@ -4,7 +4,7 @@ select
      sum(cast(total_not_used_point as decimal(18,2)))
 from dwd.dwd_member_point_expire
 where   expire_time >= '2023-01-01 00:00:00' and expire_time <= '2023-12-31 23:59:59'  and  corporation_code='701'
- and store_code='7001' and cast(total_not_used_point as decimal(18,2))>0
+ and store_code='7009' and cast(total_not_used_point as decimal(18,2))>0
 group by
     store_code
 
